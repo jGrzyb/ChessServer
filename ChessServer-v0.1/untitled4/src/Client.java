6e9a340dca7e5c276afaa02aef9968191703bf7a
jakub <jgrzyb@student.agh.edu.pl>
2023-11-11 17:58:30 +0100

    sending messages with function

--- ChessServer-v0.1/untitled4/src/Client.java
+++ ChessServer-v0.1/untitled4/src/Client.java
@@ -5,12 +5,17 @@ public class Client  implements Runnable {
     private Socket client;
     private BufferedReader in;
     private PrintWriter out;
+    public volatile String input;
     private  boolean done = false;
     public void run() {
         try {
             client = new Socket("127.0.0.1", 9999);
             out = new PrintWriter(client.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            TestInput testInput = new TestInput();
+            Thread t1 = new Thread(testInput);
+            t1.start();
 
             InputHandler inHandler = new InputHandler();
             Thread t = new Thread(inHandler);
@@ -40,20 +45,40 @@ public class Client  implements Runnable {
     class InputHandler implements Runnable {
         @Override
         public void run() {
+            while (!done) {
+                while (input == null) {
+                    Thread.onSpinWait();
+                }
+                String message = input;
+                input = null;
+                if(message.equals("/quit")) {
+                    out.println(message);
+                    shutdown();
+                    break;
+                }
+                else {
+                    out.println(message);
+                }
+            }
+        }
+    }
+
+    class TestInput implements Runnable {
+        @Override
+        public void run() {
             try {
                 BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
-                while (!done) {
-                    String message = inReader.readLine();
-                    if(message.equals("/quit")) {
-                        out.println(message);
+                while (true) {
+                    String tmp = inReader.readLine();
+                    if(tmp.equals("/quit")) {
                         inReader.close();
-                        shutdown();
+                        input = tmp;
                         break;
                     }
-                    else {
-                        out.println(message);
-                    }
+                    input = tmp;
+
                 }
+
             } catch (IOException e) {
                 shutdown();
             }
